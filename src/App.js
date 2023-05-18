b1806a742e7c09d29ea5249bee78c709b1638b56
Adarsh9044 <ada9021ada@gmail.com>
2023-05-18 17:00:14 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
